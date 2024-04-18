63c969e802e4259881123c28caa754495a945849
linxcode <linvxmusic@gmail.com>
2024-04-18 23:46:37 +0700

    feat:navigationdeks

--- src/common/constant/NavigationItem.jsx
+++ src/common/constant/NavigationItem.jsx
@@ -7,16 +7,20 @@ export const NavigationItem = [
     title: "Home",
   },
   {
-    src: "#",
+    src: "https://sourceblock.gitbook.io/sourceblock/about-sourceblock/project-objectives",
     title: "About",
   },
 
   {
-    src: "#",
+    src: "https://sourceblock.gitbook.io/sourceblock/feature/1.-ai-powered-pricing-system",
+    title: "Feature",
+  },
+  {
+    src: "https://sourceblock.gitbook.io/sourceblock/community-and-support/social-media-links",
     title: "Contact",
   },
   {
-    src: "#",
+    src: "#https://sourceblock.gitbook.io/sourceblock/faq/legal-and-financial-disclosures",
     title: "FAQ",
   },
 ];
